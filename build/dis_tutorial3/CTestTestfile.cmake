62349e027cae85752ad1719a85ab141eca510608
MarkTic15 <mark.tic15@gmail.com>
2024-04-01 16:33:00 +0200

    fix

--- build/dis_tutorial3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Documents/RINS_Project/dis_tutorial3
-# Build directory: /home/pi/Documents/RINS_Project/build/dis_tutorial3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
